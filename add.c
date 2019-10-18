9835e296ac2d045a176a618fd6507c8673b58e70
K-M-Curtis <1693379664@qq.com>
2019-10-18 17:15:28 +0800

    add 3 files

---
+++ add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int a, b, sum;
+	sum = a + b;
+	
+	printf("sum= %d\n", sum);
+	
+	return 0;
+}
---
+++ sub.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int a, b, sub;
+	sub = a - b;
+	
+	printf("sub= %d\n", sub);
+	
+	return 0;
+}

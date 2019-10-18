f5d7e03d465c4439e8e9f6fa69e2ec217b161a1f
K-M-Curtis <1693379664@qq.com>
2019-10-18 17:58:58 +0800

    modify mul.c

--- mul.c
+++ mul.c
@@ -4,7 +4,14 @@ int main(void)
 {
     int a, b, mul;
     
+	if(a>100 || b>100)
+	{
+		printf("the num is bigger\n");
+		return -1;
+	}
+	
     printf("mul= %d\n", a * b);
+	
     
     return 0;
 }
